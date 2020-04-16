bab64bccd0e9476ad69af2db8688b267812b51db
Vialeon <dnewberry21@amherst.edu>
2020-04-16 13:13:37 -0400

    pushing what I got rn

--- network.py
+++ network.py
@@ -2,7 +2,14 @@ import random as rand
 import pygame
 from visuals import *
 import time
+'''
+Things I could add atm. Passwords for each computer and a giant thing that tries all of them
+add a pseudo smtp connection with the debug mode thinger(people could've turned off debug)
+try the fingerd bug if smtp does not 'work' ie buffer overflow (note fingerd was just backwards compatible and actually fixed in 1979)
+rexec and passwords: cornell did not encrypt usernames and they wre readily available. After getting the username just use rexec
+rsh and trust: local host only knows network address for trusted connections<-- this can be spoofed
 
+'''
 class computer:
     def __init__(self,ip):
         self.trustedConnections = []
@@ -44,6 +51,9 @@ class network:
                     print("something went wrong initiating connections")
     #def initRandomConnections(self):
         #this will create random connections through certain Ip address'
+    def infectComputer(x,y):
+        self.networkComputers[y,x].timesInfected +=1
+
 
 if __name__ == "__main__":
     sample = network()
@@ -74,7 +84,6 @@ if __name__ == "__main__":
             toBlit = pygame.image.fromstring(bytes,size,mode)
             wormDisplay.blit(toBlit,(j*60+5,i*60+5))
             pygame.display.flip()
-            clock.tick(fps)
 
 
     #this will wait until I enter play to run
@@ -84,6 +93,21 @@ if __name__ == "__main__":
             break
         else:
             time.sleep(.5)
+
+    print("starting worm")
+    infectComputer(int(heightWidth/2),int(heightWidth/2))
+
+    # to start it could be more like it's just that if the guy next to you is infected you have a 1/7 chance of being infected
+    for i in range(len(sample.networkComputers)):
+        for j in range(len(sample.networkComputers[0])):
+
+
+    #try smtp if debug enabled then run command to open and run bug if succeeded the init should send a byte to some server/flip a switch
+    #if that doesn't work try fingerd if the machine is VAX this succeeds if it not then it fails
+    #if that doesn't work try rexec with the bunch of usernames you got from the the /etc/passwd file that has uncencryped usernames and the common passwords you've got
+    #if that doesn't work try rsh.. search for trusted connections and connect to said hosts if from rhosts just pass the right user name along and replicate
+
+
     ''' what I want to do is make the background, blit the computers into their positions
     also possibly make it so that it is random whether a connection is made between
     every howevermany computers. From there make a 'virus' that uses the different ideas
